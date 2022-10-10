86b47b1c895d344c2b3ac764320ba1752f26175f
Rizwanul Wahid <rizwanulwahid612@gmail.com>
2022-10-11 01:58:20 +0600

    animation added

--- src/component/Topic/Topic.js
+++ src/component/Topic/Topic.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Lottie from "lottie-react";
+import reading from '../utlity/Anemation/reading.json'
 
 const Topic = () => {
     return (
         <div>
-            
+            <Lottie style={{width:'250px' ,marginLeft:'50px'}} animationData={reading}></Lottie>
         </div>
     );
 };
